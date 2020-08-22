0524bb465d9daf39bed660521c97acc9fc26d98b
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-08-22 21:47:41 +0200

    compleet login

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const { NotExtended } = require('http-errors');
 const router = express.Router();
+
+router.all('*', (req, res, next)=> {
+  if(!req.session.admin){
+    res.redirect('login');
+
+    return;
+  }
+
+  next();
+})
 
 /* GET home page. */
 router.get('/', (req, res)=> {
