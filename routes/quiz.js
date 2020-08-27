ecca70003654566a2f6cd83ac42a91a68ff91bea
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-08-27 23:51:00 +0200

    add quiz part

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res)=> {
+// new Quiz({title: 'Pytanie 1', vote: 0}).save(); - linijka która nam utworzyła i dodała model w naszej bazie
+Quiz.find({}, (err, data) => {
+console.log(data)
+
   res.render('quiz', { title: 'Quiz' });
+})
+  
 });
 
 
